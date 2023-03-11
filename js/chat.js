1ae38c25bed4959887deab97a8be377f6bbff9ff
EntropyValley <Valerie.Harris.Personal@gmail.com>
2023-03-10 22:18:12 -0700

    Add Dummy Data and fix message creation script

--- js/chat.js
+++ js/chat.js
@@ -1,5 +1,14 @@
 const createMessage = (username, datetime, message, options={anonymous:false, currentUser:false}) => {
     const messageElement = document.createElement('div');
+
+    if (options.anonymous) {
+        username = 'Anonymous'
+        $(messageElement).addClass('anonymousMessage');
+    }
+
+    if (options.currentUser) {
+        $(messageElement).addClass('currentUserMessage');
+    }   
     
     const messageInfo = document.createElement('div');
     messageInfo.textContent = `${username}: ${datetime}`;
@@ -9,25 +18,52 @@ const createMessage = (username, datetime, message, options={anonymous:false, cu
     messageContent.textContent = message;
     $(messageContent).addClass('messageContent').appendTo($(messageElement));
 
-   $(messageElement).addClass('messageInstance').appendTo($('#chat'));
+    $(messageElement).addClass('messageInstance').appendTo($('#chat'));
 
-   if (options.anonymous) {
-    $(messageElement).addClass('anonymousMessage');
-   }
-
-   if (options.currentUser) {
-    $(messageElement).addClass('currentUserMessage');
-   }   
-
-   return messageElement;
+    return messageElement;
 }
 
 
 // Write current messages from localStorage
 let currentMessages  = localStorage.getItem('messages');
+let currentUser = localStorage.getItem('username');
 
-if (currentMessages) {
-    for (const chat of currentMessages) {
-        createMessage(chat.username, chat.datetime, chat.message);
-    }
+if (!currentMessages) {
+    currentMessages = [
+        {
+            username: 'Johnny354',
+            datetime: 'Thursday March 9th at 8:03',
+            message: 'Hello everybody! how is it going today?'
+        },
+        {
+            username: currentUser,
+            datetime: 'Thursday March 9th at 9:13',
+            message: 'I\'m doing pretty decently, how about you?'
+        },
+        {
+            username: 'Johnny354',
+            datetime: 'Thursday March 9th at 13:05',
+            message: 'I\'m doing alright.  Just another day and another day\'s worth of work.  You know how it can be.'
+        },
+        {
+            username: 'Johnny354',
+            datetime: 'Thursday March 9th at 19:02',
+            message: 'I\'m doing alright.  Wish I was doing better though.',
+            anonymous: true
+        },
+        {
+            username: currentUser,
+            datetime: 'Thursday March 9th at 9:37',
+            message: 'I definitely feel that.',
+            anonymous: true
+        }
+    ]
 }
+
+for (const chat of currentMessages) {
+    createMessage(chat.username, chat.datetime, chat.message, {anonymous: chat.anonymous, currentUser: chat.username == currentUser});
+}
+
+$('#send').click(() => {
+    
+});
