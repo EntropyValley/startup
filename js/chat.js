b4037d53805c8ccb100d99d0e79a07213f4a6547
EntropyValley <Valerie.Harris.Personal@gmail.com>
2023-03-10 21:48:03 -0700

    Add classes to messages for proper styling; add message options

--- js/chat.js
+++ js/chat.js
@@ -1,16 +1,25 @@
-const createMessage = (username, datetime, message) => {
+const createMessage = (username, datetime, message, options={anonymous:false, currentUser:false}) => {
     const messageElement = document.createElement('div');
     
     const messageInfo = document.createElement('div');
     messageInfo.textContent = `${username}: ${datetime}`;
-    $(messageInfo).appendTo($(message));
+    $(messageInfo).addClass('messageInfo').appendTo($(messageElement));
     
     const messageContent = document.createElement('div');
     messageContent.textContent = message;
-    $(messageContent).appendTo($(message));
+    $(messageContent).addClass('messageContent').appendTo($(messageElement));
 
-   $(messageElement).appendTo($('#chat'));
+   $(messageElement).addClass('messageInstance').appendTo($('#chat'));
 
+   if (options.anonymous) {
+    $(messageElement).addClass('anonymousMessage');
+   }
+
+   if (options.currentUser) {
+    $(messageElement).addClass('currentUserMessage');
+   }   
+
+   return messageElement;
 }
 
 
