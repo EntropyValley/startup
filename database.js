d240bf58f24aae30af2be9595f5413c29fc257f3
EntropyValley <Valerie.Harris.Personal@gmail.com>
2023-04-06 20:40:22 -0600

    Add message storage and retrevial functions

--- database.js
+++ database.js
@@ -8,7 +8,7 @@ const password = process.env.MONGOPASSWORD;
 const hostname = process.env.MONGOHOSTNAME;
 
 if (!userName) {
-  throw Error('Database not configured. Set environment variables');
+    throw Error('Database not configured. Set Environment Variables');
 }
 
 // Connect to MongoDB
@@ -17,6 +17,7 @@ const client = new MongoClient(url);
 
 // Database Collections
 const userCollection = client.db('startup').collection('user');
+const messageCollection = client.db('startup').collection('user');
 
 // Get a user from the database based on username
 function getUser(username) {
@@ -31,19 +32,45 @@ function getUserBySession(session) {
 // Create a user and assign a session token
 async function createUser(username, password) {
     const passwordHash = await bcrypt.hash(password, 10);
-  
+
     const user = {
-      username: username,
-      password: passwordHash,
-      session: uuid.v4(),
+        username: username,
+        password: passwordHash,
+        session: uuid.v4(),
     };
     await userCollection.insertOne(user);
-  
+
     return user;
-  }
+}
+
+// Store a message in the database and generate a client-safe representation
+async function createMessage(username, anonymous, content, epoch) {
+    const message = { username: username, anonymous: anonymous, content: content, datetime: epoch };
+    await messageCollection.insertOne(message);
+
+    // Anonymize data sent to client
+    if (!anonymous) {
+        return message
+    } else {
+        return { username: null, anonymous: true, content: content, datetime: epoch };
+    }
+}
+
+// Get the latest `num` messages from the database
+function getMessages(num = 15) {
+    const query = {}
+    const options = {
+        sort: { datetime: -1 },
+        limit: num
+    }
+    const cursor = messageCollection.find(query, options);
+    return cursor.toArray();
+}
 
 module.exports = {
     getUser,
     getUserBySession,
-    createUser
+    createUser,
+    createMessage,
+    getMessages
 }
